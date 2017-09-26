c7b538af1b462ecf4fc512d187c1493eeec2fe95
Cloud User <ec2-user@ip-172-31-16-175.us-west-2.compute.internal>
2017-09-26 15:49:11 +0000

    created separate puppet Manifest for task 1

---
+++ task_1.pp
@@ -0,0 +1,44 @@
+class task_1 {
+  
+  user { 'becca':
+    ensure => present,
+	uid => '10012192',
+	shell => '/bin/bash',
+	home => '/home/becca',
+    groups => ['sysadmin', 'cars'],
+	password => '$1$B8nO52/W$rYEDv8OdwXsPV1HJyBaqL.'
+  }
+  
+  user { 'fred':
+    ensure => present,
+	uid => '10022192',
+	shell => '/bin/csh',
+	home => '/home/fred',
+    groups => ['trucks', 'cars'],
+	password => '$1$BkNjh/g7$5mUVroTxa1CWOR3Tl2Ti4.'
+  }
+
+  user { 'wilma':
+    ensure => present,
+	uid => '10032192',
+	home => '/home/wilma',
+    groups => ['trucks', 'cars', 'ambulances'],
+	password => '$1$XqCLK6uT$6lybz7Kc5bYGyjcZihYU50'
+	
+  }
+  
+  group { 'sysadmin':
+    ensure => present,
+  }
+  
+  group { 'cars':
+    ensure => present,
+  }
+  
+  group { 'trucks':
+    ensure => present,
+  }
+  
+  group { 'ambulances':
+    ensure => present,
+  }
